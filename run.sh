2e7a67da9c1dd400ceb47dd88d38c7d14b1b9302
Robert M <rmetzger@apache.org>
2015-07-15 12:57:02 +0000

    it is working

---
+++ run.sh
@@ -0,0 +1,4 @@
+
+
+java -cp $HADOOP_CONF_DIR:target/yarn-chaos-monkey-1.0-SNAPSHOT-jar-with-dependencies.jar com.github.yarnchaosmonkey.App $@
+
--- scripts/remoteKill.sh
+++ scripts/remoteKill.sh
@@ -6,4 +6,9 @@ HOST_SHORT=$3
 
 echo "Killing $CONTAINER_ID on host $HOST"
 
-ssh -n $HOST_SHORT -- "killall $CONTAINER_ID"
+PID=`ssh  $HOST_SHORT -- ps aux | grep $CONTAINER_ID | grep java | tr -s ' ' | cut -d ' ' -f 2 | head -n 1`
+
+echo "PID = $PID"
+ssh  $HOST_SHORT -- sudo kill $PID
+
+
